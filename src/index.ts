72d89edf074803a2288edcbb9a9e4c14419a25b3
moringaman <rob@webnostix.co.uk>
2022-09-21 17:18:20 +0100

    merge fixed

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Observable } from 'rxjs';
+
+const someObservable$ = new Observable<string>(subscriber => {
+  console.log('observable executed')
+  subscriber.next('Alice');
+  setTimeout(() => subscriber.next('Ben'), 1000);
+  setTimeout(() => subscriber.next('Charlie'), 4000);
+  // subscriber.complete();
+});
+console.log("subscription 1 starts")
+const subscription = someObservable$.subscribe(value => console.log('Subscription 1 ', value));
+
+setTimeout(() => {
+console.log("subscription 2 starts")
+const subscription = someObservable$.subscribe(value => console.log('Subscription 2 ', value));
+}, 1000);
